337c19eb4fe4af5dde8ba154540364c81878c904
Andreas Weber <andreas_weber_@gmx.de>
2014-08-30 10:38:34 +0200

    added level 3, version 1.0

--- KhunPhanDroid/gen/aweber/phandroid/R.java
+++ KhunPhanDroid/gen/aweber/phandroid/R.java
@@ -24,34 +24,43 @@ public final class R {
     public static final class dimen {
         public static final int inner_height_l1=0x7f060002;
         public static final int inner_height_l2=0x7f060008;
+        public static final int inner_height_l3=0x7f06000e;
         public static final int inner_width_l1=0x7f060003;
         public static final int inner_width_l2=0x7f060009;
+        public static final int inner_width_l3=0x7f06000f;
         public static final int outer_height_l1=0x7f060004;
         public static final int outer_height_l2=0x7f06000a;
+        public static final int outer_height_l3=0x7f060010;
         public static final int outer_width_l1=0x7f060005;
         public static final int outer_width_l2=0x7f06000b;
+        public static final int outer_width_l3=0x7f060011;
         public static final int piece1_l1=0x7f060000;
         public static final int piece1_l2=0x7f060006;
+        public static final int piece1_l3=0x7f06000c;
         public static final int piece2_l1=0x7f060001;
         public static final int piece2_l2=0x7f060007;
+        public static final int piece2_l3=0x7f06000d;
     }
     public static final class drawable {
         public static final int background_inner=0x7f020000;
         public static final int background_inner2=0x7f020001;
-        public static final int background_outer=0x7f020002;
-        public static final int background_outer2=0x7f020003;
-        public static final int button_selectable=0x7f020004;
-        public static final int button_shape=0x7f020005;
-        public static final int icon=0x7f020006;
-        public static final int piece11_pic=0x7f020007;
-        public static final int piece12_pic=0x7f020008;
-        public static final int piece21_pic=0x7f020009;
-        public static final int piece22_pic=0x7f02000a;
-        public static final int start_pic=0x7f02000b;
+        public static final int background_inner3=0x7f020002;
+        public static final int background_outer=0x7f020003;
+        public static final int background_outer2=0x7f020004;
+        public static final int background_outer3=0x7f020005;
+        public static final int button_selectable=0x7f020006;
+        public static final int button_shape=0x7f020007;
+        public static final int icon=0x7f020008;
+        public static final int piece11_pic=0x7f020009;
+        public static final int piece12_pic=0x7f02000a;
+        public static final int piece21_pic=0x7f02000b;
+        public static final int piece22_pic=0x7f02000c;
+        public static final int start_pic=0x7f02000d;
     }
     public static final class id {
         public static final int InnerBoardLayout=0x7f090004;
         public static final int InnerBoardLayout2=0x7f090013;
+        public static final int InnerBoardLayout3=0x7f090028;
         public static final int L2Piece11_1=0x7f090014;
         public static final int L2Piece11_2=0x7f090015;
         public static final int L2Piece11_3=0x7f090016;
@@ -68,10 +77,25 @@ public final class R {
         public static final int L2Piece21_4=0x7f090021;
         public static final int L2Piece21_5=0x7f090022;
         public static final int L2Piece22=0x7f090023;
+        public static final int L3Piece11_1=0x7f090029;
+        public static final int L3Piece11_2=0x7f09002a;
+        public static final int L3Piece12_1=0x7f09002b;
+        public static final int L3Piece12_2=0x7f09002c;
+        public static final int L3Piece12_3=0x7f09002d;
+        public static final int L3Piece12_4=0x7f09002e;
+        public static final int L3Piece12_5=0x7f09002f;
+        public static final int L3Piece12_6=0x7f090030;
+        public static final int L3Piece21_1=0x7f090031;
+        public static final int L3Piece21_2=0x7f090032;
+        public static final int L3Piece21_3=0x7f090033;
+        public static final int L3Piece21_4=0x7f090034;
+        public static final int L3Piece22=0x7f090035;
         public static final int Level1Layout=0x7f090000;
         public static final int Level2Layout=0x7f09000f;
+        public static final int Level3Layout=0x7f090024;
         public static final int OuterBoardLayout=0x7f090003;
         public static final int OuterBoardLayout2=0x7f090012;
+        public static final int OuterBoardLayout3=0x7f090027;
         public static final int Piece11_1=0x7f090005;
         public static final int Piece11_2=0x7f090006;
         public static final int Piece11_3=0x7f090007;
@@ -82,21 +106,25 @@ public final class R {
         public static final int Piece12_4=0x7f09000c;
         public static final int Piece21=0x7f09000d;
         public static final int Piece22=0x7f09000e;
-        public static final int button_level1=0x7f090024;
-        public static final int button_level2=0x7f090025;
-        public static final int opt_about=0x7f090029;
-        public static final int opt_finish=0x7f090028;
-        public static final int opt_restart=0x7f090026;
-        public static final int opt_sound=0x7f090027;
+        public static final int button_level1=0x7f090036;
+        public static final int button_level2=0x7f090037;
+        public static final int button_level3=0x7f090038;
+        public static final int opt_about=0x7f09003c;
+        public static final int opt_finish=0x7f09003b;
+        public static final int opt_restart=0x7f090039;
+        public static final int opt_sound=0x7f09003a;
         public static final int txt_best_solution=0x7f090002;
         public static final int txt_best_solution2=0x7f090011;
+        public static final int txt_best_solution3=0x7f090026;
         public static final int txt_moves=0x7f090001;
         public static final int txt_moves2=0x7f090010;
+        public static final int txt_moves3=0x7f090025;
     }
     public static final class layout {
         public static final int level1=0x7f030000;
         public static final int level2=0x7f030001;
-        public static final int start=0x7f030002;
+        public static final int level3=0x7f030002;
+        public static final int start=0x7f030003;
     }
     public static final class menu {
         public static final int game_menu=0x7f080000;
@@ -109,16 +137,17 @@ public final class R {
         public static final int app_name=0x7f070000;
         public static final int button_level1=0x7f070001;
         public static final int button_level2=0x7f070002;
-        public static final int button_new_game=0x7f070003;
-        public static final int dialog_about=0x7f07000c;
-        public static final int dialog_help=0x7f07000d;
-        public static final int dialog_success=0x7f070004;
-        public static final int game_best=0x7f07000b;
-        public static final int game_moves=0x7f07000a;
-        public static final int menu_about=0x7f070009;
-        public static final int menu_exit=0x7f070005;
-        public static final int menu_restart=0x7f070006;
-        public static final int menu_sound_off=0x7f070008;
-        public static final int menu_sound_on=0x7f070007;
+        public static final int button_level3=0x7f070003;
+        public static final int button_new_game=0x7f070004;
+        public static final int dialog_about=0x7f07000d;
+        public static final int dialog_help=0x7f07000e;
+        public static final int dialog_success=0x7f070005;
+        public static final int game_best=0x7f07000c;
+        public static final int game_moves=0x7f07000b;
+        public static final int menu_about=0x7f07000a;
+        public static final int menu_exit=0x7f070006;
+        public static final int menu_restart=0x7f070007;
+        public static final int menu_sound_off=0x7f070009;
+        public static final int menu_sound_on=0x7f070008;
     }
 }
--- KhunPhanDroid/src/aweber/phandroid/StartActivity.java
+++ KhunPhanDroid/src/aweber/phandroid/StartActivity.java
@@ -10,6 +10,8 @@ public class StartActivity extends Activity {
 	private static final int SUB_ACTIVITY_REQUEST_CODE_L1 = 13;
 
 	private static final int SUB_ACTIVITY_REQUEST_CODE_L2 = 14;
+	
+	private static final int SUB_ACTIVITY_REQUEST_CODE_L3 = 15;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -38,13 +40,25 @@ public class StartActivity extends Activity {
 		// we can't use startActivity() cause we want to react on child activity's finish (see onActivityResult())
 		startActivityForResult(i, SUB_ACTIVITY_REQUEST_CODE_L2);
 	}
+	
+	/**
+	 * Called when clicking Level3-Button
+	 * 
+	 * @see res/layout/start.xml
+	 */
+	public void onClickStartLevel3(final View v) {
+		final Intent i = new Intent(this, Level3Activity.class);
+		// we can't use startActivity() cause we want to react on child activity's finish (see onActivityResult())
+		startActivityForResult(i, SUB_ACTIVITY_REQUEST_CODE_L3);
+	}
 
 	/** Called when child activity finishes. */
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		super.onActivityResult(requestCode, resultCode, data);
 		if ((requestCode == SUB_ACTIVITY_REQUEST_CODE_L1 && resultCode == Level1Activity.EXIT_RETURN_CODE)
-				|| (requestCode == SUB_ACTIVITY_REQUEST_CODE_L2 && resultCode == Level2Activity.EXIT_RETURN_CODE)) {
+				|| (requestCode == SUB_ACTIVITY_REQUEST_CODE_L2 && resultCode == Level2Activity.EXIT_RETURN_CODE)
+				|| (requestCode == SUB_ACTIVITY_REQUEST_CODE_L3 && resultCode == Level3Activity.EXIT_RETURN_CODE)) {
 			finish(); // 'Beenden' has been chosen in child activity's option menu -> also close parent(=this) activity
 		}
 	}
